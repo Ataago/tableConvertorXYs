8b551ab586d28800df34c199e3e188a9c2c087d5
ataago <ataago7@gmail.com>
2023-12-16 11:04:37 +0000

    refactor code in build_worldcloud.

--- scripts/build_wordcloud.py
+++ scripts/build_wordcloud.py
@@ -7,31 +7,34 @@ from wordcloud import WordCloud
 path = '/Users/ataago/Documents/git/tableConvertorXYs/input/'
 out_path = '/Users/ataago/Documents/git/tableConvertorXYs/output/'
 
+
+def gen_wordcloud(freq, out_file_path):
+
+    # Generate a word cloud for 'Sustainable Practice'
+    wordcloud = WordCloud(
+        width=1600, height=800, background_color='white'
+    ).generate_from_frequencies(frequencies=freq)
+
+    # Display the word cloud
+    plt.figure(figsize=(20, 10))
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.tight_layout()
+    # plt.title('Word Cloud for Sustainable Practice', fontweight='bold', fontsize=20, color='darkred')
+
+    # Save the word cloud as a .png file
+    plt.savefig(out_file_path, bbox_inches='tight')
+
+
 # Reload the data
 data = pd.read_excel(path + "PracticesWithImpacts(edited2).xlsx", header=0)
 
-# Generate a word cloud for 'Sustainable Practice'
-wordcloud_practice = WordCloud(width=1600, height=800, background_color='white').generate_from_frequencies(data['Sustainable Practice'].value_counts())
+gen_wordcloud(
+    freq=data['Sustainable Practice'].value_counts(),
+    out_file_path=out_path + 'wordcloud_sustainable_practice.png'
+)
 
-# Display the word cloud
-plt.figure(figsize=(20, 10))
-plt.imshow(wordcloud_practice, interpolation='bilinear')
-plt.axis('off')
-# plt.title('Word Cloud for Sustainable Practice', fontweight='bold', fontsize=20, color='darkred')
-# Save the word cloud as a .png file
-plt.tight_layout()
-plt.savefig(out_path + 'wordcloud_sustainable_practice.png')
-plt.show()
-
-# Generate a word cloud for 'Performance Outcome'
-wordcloud_outcome = WordCloud(width=1600, height=800, background_color='white').generate_from_frequencies(data['Performance Outcome'].value_counts())
-
-# Display the word cloud
-plt.figure(figsize=(20, 10))
-plt.imshow(wordcloud_outcome, interpolation='bilinear')
-plt.axis('off')
-# plt.title('Word Cloud for Performance Outcome', fontweight='bold', fontsize=20, color='darkred')
-plt.tight_layout()
-# Save the word cloud as a .png file
-plt.savefig(out_path + 'wordcloud_performance_outcome.png')
-plt.show()
+gen_wordcloud(
+    freq=data['Performance Outcome'].value_counts(),
+    out_file_path=out_path + 'wordcloud_performance_outcome.png'
+)
